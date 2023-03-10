9dcf0f378b0fb23ca2681ab8e590bcd47758ef55
messele ghebreslassie <messeleghebreslassie@gmail.com>
2023-03-09 20:04:06 -0700

    completed tut #3 for django tut

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+posts = [
+    {
+        "author":"CoreyMS",
+        "title":"Blog Post 1",
+        "content":"First post content",
+        "date_posted":"August 23, 2018"    
+    },
+    {
+        "author":"Jane Doe",
+        "title":"Blog Post 2",
+        "content":"Second post content",
+        "date_posted":"June 18, 2018"    
+    }
+]
+
+def home(request):
+    context = {
+        "posts":posts
+    }
+    return render(request, "blog/home.html", context)
+
+def about(request):
+    return render(request, "blog/about.html", context={"title":"About"})
